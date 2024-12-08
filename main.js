aac9dd528cc54e685a3c0a3d5a036caa5787e0e4
Sam <samuv.eth@gmail.com>
2024-12-08 13:00:11 +0700

    add faq

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 import { html, render } from "lit-html";
+import { FaqPage } from "./faq.js";
 
-const template = () => html`
+// Shared Components
+const Footer = () => html`
+  <footer class="footer">
+    <div class="social-links">
+      <a
+        href="https://t.me/fungibles_ERC20i"
+        class="social-link"
+        target="_blank"
+        rel="noopener"
+        >Telegram</a
+      >
+      <a
+        href="https://github.com/fungiblesxyz"
+        class="social-link"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+      <a
+        href="https://x.com/fungiblesxyz"
+        class="social-link"
+        target="_blank"
+        rel="noopener"
+        >X.com</a
+      >
+      <a
+        href="https://app.safe.global/balances?safe=base:0x45083345B7E20d9916dDa046344Ec518bf9e21D0"
+        class="social-link"
+        target="_blank"
+        rel="noopener"
+        >Support Us</a
+      >
+    </div>
+  </footer>
+`;
+
+// Page Components
+const homePage = () => html`
   <div class="container">
     <img
       src="https://pbs.twimg.com/profile_images/1858456285789978624/6zRI1BEe_400x400.jpg"
@@ -45,40 +83,28 @@ const template = () => html`
         <img src="/icons/bot.svg" alt="" class="icon" />
         Telegram Bot
       </a>
+      <a href="#faq" class="action-link">
+        <img src="/icons/question.svg" alt="" class="icon" />
+        FAQ
+      </a>
     </div>
-    <footer class="footer">
-      <div class="social-links">
-        <a
-          href="https://t.me/fungibles_ERC20i"
-          class="social-link"
-          target="_blank"
-          rel="noopener"
-          >Telegram</a
-        >
-        <a
-          href="https://github.com/fungiblesxyz"
-          class="social-link"
-          target="_blank"
-          rel="noopener"
-          >GitHub</a
-        >
-        <a
-          href="https://x.com/fungiblesxyz"
-          class="social-link"
-          target="_blank"
-          rel="noopener"
-          >X.com</a
-        >
-        <a
-          href="https://app.safe.global/balances?safe=base:0x45083345B7E20d9916dDa046344Ec518bf9e21D0"
-          class="social-link"
-          target="_blank"
-          rel="noopener"
-          >Support Us</a
-        >
-      </div>
-    </footer>
+    ${Footer()}
   </div>
 `;
 
-render(template(), document.querySelector("#app"));
+// Routing
+const routes = {
+  home: homePage,
+  faq: () => FaqPage(Footer),
+};
+
+let currentPage = window.location.hash.slice(1) || "home";
+
+const handleRoute = () => {
+  currentPage = window.location.hash.slice(1) || "home";
+  render(routes[currentPage](), document.querySelector("#app"));
+};
+
+// Initialize
+window.addEventListener("hashchange", handleRoute);
+handleRoute();
